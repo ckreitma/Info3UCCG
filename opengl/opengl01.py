8a669acb8223a1983f521836364887ba2c4ce8e1
Mauricio Kreitmayr <ckreitma@gmail.com>
2021-09-08 10:01:45 -0400

    Opengl02

--- opengl/opengl01.py
+++ opengl/opengl01.py
@@ -27,7 +27,7 @@ def showScreen():
 def opengl01():
     glutInit()  # Inicializa lal instancia OpenGL
     glutInitDisplayMode(GLUT_RGBA)  # Eligiendo el modo en el cual se van a colorear los objetos
-    glutInitWindowSize(500, 500)   # Set the width and height of your window
+    glutInitWindowSize(900, 900)   # Set the width and height of your window
     glutInitWindowPosition(0, 0)   # Set the position at which this windows should appear
     wind = glutCreateWindow("OpenGL Coding Practice")  # Give your window a title
     glutDisplayFunc(showScreen)  # Tell OpenGL to call the showScreen method continuously
